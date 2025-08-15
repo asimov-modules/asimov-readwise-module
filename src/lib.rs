4b1268bfb50407626792a32e2074fb86a5d3bfb0
˜Luan <luannlopessilva@outlook.com>
2025-08-15 08:05:33 -0300

    feat: add core library module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
 // This is free and unencumbered software released into the public domain.
 
-#![no_std]
 #![forbid(unsafe_code)]
+
+pub mod api;
+pub mod jq;
+pub mod providers;
+
+pub use providers::Provider;
+
+pub fn find_engine_for(url: impl AsRef<str>) -> Option<&'static Provider> {
+    let url = url.as_ref();
+    for (url_pattern, engine) in providers::readwise::URL_PREFIX_TO_PROVIDER.iter().rev() {
+        if url.starts_with(url_pattern) {
+            return Some(engine);
+        }
+    }
+    None // not found
+}
