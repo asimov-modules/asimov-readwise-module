04d5bfc706792350235908c8d40911928deaa7f6
˜Luan <luannlopessilva@outlook.com>
2025-08-16 02:00:56 -0300

    fix(lib): change variable name

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,12 @@ pub mod providers;
 
 pub use providers::Provider;
 
-pub fn find_engine_for(url: impl AsRef<str>) -> Option<&'static Provider> {
+pub fn find_provider_for(url: impl AsRef<str>) -> Option<&'static Provider> {
     let url = url.as_ref();
-    for (url_pattern, engine) in providers::readwise::URL_PREFIX_TO_PROVIDER.iter().rev() {
+    for (url_pattern, provider) in providers::readwise::URL_PREFIX_TO_PROVIDER.iter().rev() {
         if url.starts_with(url_pattern) {
-            return Some(engine);
+            return Some(provider);
         }
     }
-    None // not found
+    None
 }
